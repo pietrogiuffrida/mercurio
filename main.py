738f84b5b627cfe936c8b45ddc100ced8648f8c5
Pietro Giuffrida <p.giuffrida@crif.com>
2019-07-18 19:42:16 +0200

    Debug + readme

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ if __name__ == '__main__':
         smtp_pwd="mypwd"
     )
 
-    hg.send(
-        fromaddress="from@me.com",
-        recipients=["to@me.com"],
-        reply_to_address="reply@to.me",
-        text="a long text, very long.",
-        sbj="The subject, or the object?",
-        attachments=[],
-        cid_attachments={"content_id_01": "mylogo.png"}
-    )
-
     # to debug purposes, you can create the message and save it on disk
     # without send it thgrougth smtp.
     message = hg.make_message(
         fromaddress="from@me.com",
-        recipients=["to@me.com"],
+        recipient="to@me.com",
         reply_to_address="reply@to.me",
         text="a long text, very long.",
         sbj="The subject, or the object?",
         attachments=[],
-        cid_attachments={"content_id_01": "mylogo.png"}
+        cid_attachments={}
     )
 
     with open('test_email.eml', 'w') as o:
         o.write(message.as_string())
+
+    # hg.send(
+    #     fromaddress="from@me.com",
+    #     recipients=["to@me.com"],
+    #     reply_to_address="reply@to.me",
+    #     text="a long text, very long.",
+    #     sbj="The subject, or the object?",
+    #     attachments=[],
+    #     cid_attachments={"content_id_01": "mylogo.png"}
+    # )
